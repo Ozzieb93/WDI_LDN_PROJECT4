0bc8e0e303cb0018ef7cf742fa185ef7da87eec1
Usman Balesaria <ubalesaria@me.com>
2018-05-24 13:52:37 +0100

    Added the SASS file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Styling
+import './scss/style.scss';
+
 class App extends React.Component {
   render() {
     return (
-      <h1>WDI32 Project 4</h1>
+      <h1>WDI33 Project 4</h1>
     );
   }
 }
