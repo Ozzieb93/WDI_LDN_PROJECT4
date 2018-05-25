141cba3f12c0b22d9158cab67649e2b134716346
Usman Balesaria <ubalesaria@me.com>
2018-05-25 10:30:02 +0100

    Added the correct routes for jobs

--- config/router.js
+++ config/router.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const auth = require('../controllers/auth');
+const jobs = require('../controllers/jobs');
 // const secureRoute = require('../lib/secureRoute');
 
+router.route('/jobs')
+  .get(jobs.index)
+  .post( jobs.create);
 
+router.route('/jobs/:id')
+  .get(jobs.show)
+  .put( jobs.update)
+  .delete( jobs.delete);
 
 router.post('/register', auth.register);
 router.post('/login', auth.login);
