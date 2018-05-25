653bb07397459cacecc9f75a8eb677463169ecbf
Usman Balesaria <ubalesaria@me.com>
2018-05-25 15:32:38 +0100

    Made more components for the SASS files

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ class App extends React.Component {
     return (
       <Router>
         <main>
+          {/* <Navbar /> */}
           <section className="section">
             <div className="container">
               <Switch>
@@ -26,7 +27,6 @@ class App extends React.Component {
           </section>
         </main>
       </Router>
-
     );
   }
 }
