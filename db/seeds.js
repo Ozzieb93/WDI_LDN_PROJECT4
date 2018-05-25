6008293e75720fb89f0a45ae2634043457ab6de7
Usman Balesaria <ubalesaria@me.com>
2018-05-25 11:07:48 +0100

    Added property seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 const { dbURI } = require('../config/environment');
 const Job = require('../models/Job');
+const Property = require('../models/property');
 
 mongoose.connect(dbURI, (err, db) => {
   db.dropdatabe();
@@ -13,7 +14,18 @@ mongoose.connect(dbURI, (err, db) => {
     description: 'Boiler is broken',
     image: 'https://www.vaillant.co.uk/images/ecofit-sustain-2/whbc16-13692-01-890027-format-3-4@570@desktop.jpg'
   }])
-    .then(jobs => console.log(`${jobs.length} jobs created`))
+    .then(jobs => console.log(`${jobs.length} jobs created`));
+
+  return Property
+    .create([{
+      address: '14 West Bank, London, N16 5DG',
+      tenants: 4,
+      image: 'https://i.imgur.com/Urseki1.png'
+    } ,{
+      address: '98 Dynevour Road',
+      tenants: 7,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTsF52cBB_D0Dvga_JyFkK447NbSeHMzr2m7vrrE8yBMfvW0fILQ'
+    }])
     .catch(err => console.log(err))
     .finally(() => mongoose.connection.close());
 });
