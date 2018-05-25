25cf2004f1c5477dd2ece81ebf62079fa5712c7c
Usman Balesaria <ubalesaria@me.com>
2018-05-25 09:22:52 +0100

    Added the user model

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+const bodyParser = require('body-parser');
+const { port, dbURI } = require('./config/environment');
+// const router = require('./config/router');
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect(dbURI);
+
+app.use(bodyParser.json());
 
 app.use(express.static(`${__dirname}/public`));
 
+// app.use('/api', router);
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true},
+  email: { type: String, required: true},
+  password: { type: String, required: true}
+});
+
+userSchema.set('toJSON', {
+  transform(doc, json) {
+    delete json.password;
+    return json;
+  }
+});
+
+userSchema.methods.validatePassword = function validatePassword(password){
+  return bcrypt.compareSync(password, this.password);
+  // This.password is the hashed password. password is the user input password. They are compared and then sent to the controller where it is compared and verified using the users email and the unique validator.
+};
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation){
+    this._passwordConfirmation = passwordConfirmation;
+    // The underscore is implying the passwordConfirmation is a tempory variable.
+  });
+
+//Before (pre) any function 'saves' something, run this function to encrypt the password before it is stored:
+userSchema.pre('validate', function checkPassword(next){
+  if(this.isModified('password') && this._passwordConfirmation !== this.password){
+    this.invalidate('passwordConfirmation', 'does not match');
+  }
+  next();
+});
+
+userSchema.pre('save', function hashPassword(next){
+  if(this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
