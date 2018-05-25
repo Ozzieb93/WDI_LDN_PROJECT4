c8d11e1e95279bf931efdba5005e944d727fa0f7
Usman Balesaria <ubalesaria@me.com>
2018-05-25 11:14:14 +0100

    Added the routes for the properties and users

--- config/router.js
+++ config/router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const auth = require('../controllers/auth');
 const jobs = require('../controllers/jobs');
+const properties = require('../controllers/properties');
+const users = require('../controllers/users');
 // const secureRoute = require('../lib/secureRoute');
+
+//JOBS ROUTE
 
 router.route('/jobs')
   .get(jobs.index)
@@ -11,6 +15,28 @@ router.route('/jobs/:id')
   .get(jobs.show)
   .put( jobs.update)
   .delete( jobs.delete);
+
+// PROPERTIES ROUTE
+
+router.route('/properties')
+  .get(properties.index)
+  .post( properties.create);
+
+router.route('/properties/:id')
+  .get(properties.show)
+  .put( properties.update)
+  .delete( properties.delete);
+
+// USERS ROUTE
+
+router.route('/users')
+  .get(users.index)
+  .post( users.create);
+
+router.route('/users/:id')
+  .get(users.show)
+  .put( users.update)
+  .delete( users.delete);
 
 router.post('/register', auth.register);
 router.post('/login', auth.login);
